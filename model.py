5c485235e12b2c9bbf0207b9e832869aedb502db
Timofey Gradov <timagradov@yahoo.com>
2021-01-14 11:08:13 +0300

    Minor fixes, search_new_job, pay_wages

--- model.py
+++ model.py
@@ -9,8 +9,10 @@ class Householder(Agent):
         self.wealth = random.randint(100, 1000)
         self.wage = 0
         self.companies = []
-        for company in range(7):
-            self.companies.append(self.random.choice(self.model.cmp_schedule.agents).unique_id)
+        self.company = None
+        self.wage_decreasing_coefficient = 0.95
+        for _ in range(7):
+            self.companies.append(random.choice(model.cmp_schedule.agents).unique_id)
 
     def search_cheaper_prices(self):
         pass
@@ -19,7 +21,12 @@ class Householder(Agent):
         pass
 
     def search_new_job(self):
-        pass
+        max_wage_company = sorted(self.companies, key=lambda x: x.wage, reverse=True)[0]
+        if max_wage_company.wage > self.wage:
+            max_wage_company.householders.append(self)
+            self.company = max_wage_company
+        if self.company is None:
+            self.wage *= self.wage_decreasing_coefficient
 
     def identify_consumption(self):
         pass
@@ -27,15 +34,11 @@ class Householder(Agent):
     def buy_goods(self):
         pass
 
-    def apply(self):
-        pass
-
     def end_of_month(self):
         self.search_cheaper_prices()
         self.search_productive_firms()
         self.search_new_job()
         self.identify_consumption()
-        pass
 
     def step(self):
         if self.model.current_day // 30 == 0:
@@ -49,7 +52,7 @@ class Company(Agent):
         self.wealth = random.randint(1000, 10000)
         self.wage = random.randint(100, 1000)
         self.price = 10
-        self.looking_for_worker = 0
+        self.looking_for_worker = False
         self.full_workplaces = 0
         self.demand = 10
         self.demand_min_coefficient = 0.25
@@ -62,14 +65,16 @@ class Company(Agent):
         self.tau = 0.75  # chance to increase a price
         self.upsilon = 0.02  # max range of distribution for increasing price
         self.lambda_coefficient = 3  # how many products produced by one household per day
-        self.households = {}
+        self.households = []
         self.product_price = random.randint(10, 100)
 
     def produce(self):
         pass
 
     def pay_wages(self):
-        pass
+        for h in self.households:
+            h.wealth += self.wage
+            self.wealth -= self.wage
 
     def fill_money_buffer(self):
         pass
@@ -80,7 +85,7 @@ class Company(Agent):
     # If we didn't find worker last month then increase wage
     # If we didn't loose worker for last <full_workplaces> months then decrease wage
     def set_wage_rate(self):
-        if self.looking_for_worker == 1:
+        if self.looking_for_worker:
             self.wage = self.wage * (1 + random.uniform(0, self.sigma))
         if self.full_workplaces > self.gamma:
             self.wage = self.wage * (1 - random.uniform(0, self.sigma))
@@ -89,8 +94,12 @@ class Company(Agent):
     # If we have a lot of inventory in buffer then fire one
     def hire_or_fire(self):
         if self.inventory < self.demand_min_coefficient * self.demand:
-            self.looking_for_worker = 1
+            self.looking_for_worker = True
+        else:
+            self.looking_for_worker = False
         if self.inventory > self.demand_max_coefficient * self.demand:
+            fired_h = self.households[0]
+            fired_h.company = None
             del self.households[0]
 
     # Marginal cost is price that we spend on production of 1 unit of inventory.
@@ -114,7 +123,6 @@ class Company(Agent):
         self.set_wage_rate()
         self.hire_or_fire()
         self.change_goods_price()
-        pass
 
     def step(self):
         self.produce()
