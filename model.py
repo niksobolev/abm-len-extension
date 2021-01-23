a87baeeac82463946e8d162ed0e3c989c3a3cbeb
Timofey Gradov <timagradov@yahoo.com>
2021-01-23 19:34:45 +0300

    Penalties fix

--- model.py
+++ model.py
@@ -39,15 +39,16 @@ class Householder(Agent):
             if firm not in self.companies:
                 firm_dict[firm] = len(firm.households)
         sorted_households = sorted(firm_dict.items(), key=lambda x: x[1])
-        company_to_add = draw_company(sorted_households)
-        self.companies.append(company_to_add)
+        if sorted_households:
+            company_to_add = draw_company(sorted_households)
+            self.companies.append(company_to_add)
 
     def search_productive_firms(self):
         if random.random() < self.prob_search_prod:
             sorted_penalties = sorted(self.penalty_companies.items(), key=lambda x: x[1])
             company_to_delete = draw_company(sorted_penalties)
-            self.companies.remove(company_to_delete)
             self.add_firm_by_households()
+            self.companies.remove(company_to_delete)
 
     def search_new_job(self):
         for i in range(self.unemployed_attempts):
@@ -95,11 +96,16 @@ class Householder(Agent):
                 company.inventory -= self.consumption
                 break
 
+    def update_penalties(self):
+        for company in self.companies:
+            self.penalty_companies[company] = 0
+
     def end_of_month(self):
         self.search_cheaper_prices()
         self.search_productive_firms()
         self.search_new_job()
         self.identify_consumption()
+        self.update_penalties()
 
     def step(self):
         if self.model.current_day % 30 == 0:
